b6533f427bfdaca8bf1d0ea3b69ca7f09abe05c7
yinren <yinren@192.168.7.101>
2019-04-25 01:35:18 +0800

    synchronized

--- src/com/coursereservation/controller/CourseReservationServlet.java
+++ src/com/coursereservation/controller/CourseReservationServlet.java
@@ -1,6 +1,7 @@
 package com.coursereservation.controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.sql.Timestamp;
 import java.util.LinkedList;
 import java.util.List;
@@ -12,126 +13,144 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.course.model.CourseService;
+import com.course.model.CourseVO;
 import com.coursereservation.model.CourseReservationService;
 import com.coursereservation.model.CourseReservationVO;
- 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.inscourse.model.InsCourseService;
+import com.inscourse.model.InsCourseVO;
+import com.inscoursetime.model.InsCourseTimeService;
+import com.inscoursetime.model.InsCourseTimeVO;
+
 @WebServlet("/CourseReservationServlet")
 public class CourseReservationServlet extends HttpServlet {
-	
+	private static final long serialVersionUID = 1L;
+
 	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 		doPost(req, res);
 	}
 
 	protected void doPost(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
 
+		Gson gson = new GsonBuilder().setDateFormat("yyyy-MM-dd HH:mm:ss").create();
 		req.setCharacterEncoding("UTF-8");
+		res.setContentType("text/plain; charset=UTF-8");
+		PrintWriter out = res.getWriter();
+
 		String action = req.getParameter("action");
+		System.out.println(action);
 
-		  if ("addOrder".equals(action)) { // 來自addEmp.jsp的請求  
-				
-				List<String> errorMsgs = new LinkedList<String>();
-				// Store this set in the request scope, in case we need to
-				// send the ErrorPage view.
-				req.setAttribute("errorMsgs", errorMsgs); 
-				
-
-				try {
-					/***********************1.接收請求參數 - 輸入格式的錯誤處理*************************/
-					String teacherId = req.getParameter("teacherId").trim();
-//					String memId = req.getParameter("memId").trim();
-					String inscId = req.getParameter("inscId").trim();
-
-					
-					Timestamp crvMFD = null;
-					try {
-						crvMFD = Timestamp.valueOf(req.getParameter("crvMFD").trim());
-					} catch (IllegalArgumentException e) {
-						crvMFD=new Timestamp(System.currentTimeMillis());
-						errorMsgs.add("請輸入日期!");
-					}
-			 		
-					String TeamId =null;
-					
-					Timestamp crvEXP = null;
-					try {
-						crvEXP = Timestamp.valueOf(req.getParameter("crvEXP").trim());
-					} catch (IllegalArgumentException e) {
-						crvEXP=new Timestamp(System.currentTimeMillis());
-						errorMsgs.add("請輸入日期!");
-					}
-					
-					
-					String crvLoc = req.getParameter("crvLoc").trim();
+		// 我的預約
+		if ("find_my_reservation".equals(action)) {
 			
-					Double crvTotalTime = null;
 
-						crvTotalTime = new Double(req.getParameter("crvTotalTime"));
-					
-					Double crvTotalPrice = null;
-					try {
-						crvTotalPrice = new Double(req.getParameter("crvTotalPrice"));
-					} catch (NumberFormatException e) {
-						crvTotalPrice = 0.0;
-						errorMsgs.add("請選擇時間!");
-					}
-					String teamId =null;
-					Integer crvStatus = 1;
-					Integer classStatus = 0;
-					Integer tranStatus = 0;
-					String crvRate = null;
-					String xxx= "weshare05";
-					
-					CourseReservationVO courseReservationVO=new CourseReservationVO();
-					courseReservationVO.setTeacherId(teacherId);
-					courseReservationVO.setMemId(xxx);
-					courseReservationVO.setInscId(inscId);
-					courseReservationVO.setTeamId(teamId);
-					courseReservationVO.setCrvStatus(crvStatus);
-					courseReservationVO.setClassStatus(classStatus);
-					courseReservationVO.setTranStatus(tranStatus);
-					courseReservationVO.setCrvMFD(crvMFD);
-					courseReservationVO.setCrvEXP(crvEXP);
-					courseReservationVO.setCrvLoc(crvLoc);
-					courseReservationVO.setCrvTotalTime(crvTotalTime);
-					courseReservationVO.setCrvTotalPrice(crvTotalPrice);
-					courseReservationVO.setCrvRate(crvRate);
-					// Send the use back to the form, if there were errors
-					if (!errorMsgs.isEmpty()) {
-						req.setAttribute("courseReservationVO", courseReservationVO); // 含有輸入格式錯誤的empVO物件,也存入req
-						RequestDispatcher failureView = req
-								.getRequestDispatcher("/coursereservation/courseOrder.jsp");
-						failureView.forward(req, res);
-						return;
-						
-					}
-					
-					/***************************2.開始新增資料***************************************/
-					CourseReservationService crSvc = new CourseReservationService();
-					crSvc.addCourseReservation(teacherId, xxx, inscId, teamId, crvStatus, classStatus, tranStatus, crvMFD, crvEXP, crvLoc, crvTotalTime, crvTotalPrice,  crvRate);
-					/***************************3.新增完成,準備轉交(Send the Success view)***********/
+			String param = req.getParameter("param");
+			CourseReservationService crSvc = new CourseReservationService();
+			List<CourseReservationVO> crList = crSvc.findByPrimaryKey(param);
+
+			// JOIN
+			InsCourseService inscSvc = new InsCourseService();
+			CourseService courseSvc = new CourseService();
+
+			for (CourseReservationVO crVO : crList) {
+				InsCourseVO inscVO = inscSvc.findOneById(crVO.getInscId());
+				CourseVO courseVO = courseSvc.findOneById(inscVO.getCourseId());
+				crVO.setInscId(courseVO.getCourseName());
+			}
+			out.print(gson.toJson(crList));
+
+		}
+
+		// 預約
+		if ("make_new_reservation".equals(action) || "addOrder".equals(action)) {
+			synchronized(this) {
+			CourseReservationVO crVO;
+			// Android取資料
+			if ("make_new_reservation".equals(action)) {
+				crVO = gson.fromJson(req.getParameter("crVO"), CourseReservationVO.class);
+				// Web取資料
+			} else {
+				String teacherId = req.getParameter("teacherId").trim();
+				String memId = req.getParameter("memId").trim();
+				String inscId = req.getParameter("inscId").trim();
+				Timestamp crvMFD = Timestamp.valueOf(req.getParameter("crvMFD").trim());
+				Timestamp crvEXP = Timestamp.valueOf(req.getParameter("crvEXP").trim());
+				String crvLoc = req.getParameter("crvLoc").trim();
+				Double crvTotalTime = new Double(req.getParameter("crvTotalTime"));
+				Double crvTotalPrice = new Double(req.getParameter("crvTotalPrice"));
+
+				crVO = new CourseReservationVO();
+				crVO.setTeacherId(teacherId);
+				crVO.setMemId(memId);
+				crVO.setInscId(inscId);
+				crVO.setTeamId(null);
+				crVO.setCrvStatus(1);
+				crVO.setClassStatus(0);
+				crVO.setTranStatus(0);
+				crVO.setCrvMFD(crvMFD);
+				crVO.setCrvEXP(crvEXP);
+				crVO.setCrvLoc(crvLoc);
+				crVO.setCrvTotalTime(crvTotalTime);
+				crVO.setCrvTotalPrice(crvTotalPrice);
+				crVO.setCrvRate(null);
+			}
+
+			// 確認是否已被訂走
+			InsCourseTimeService insctSvc = new InsCourseTimeService();
+			List<InsCourseTimeVO> ictVO = insctSvc.findByKey(crVO.getInscTimeId());
+			if (ictVO.size() != 0) {
+
+				// 開始新增資料
+				CourseReservationService crSvc = new CourseReservationService();
+				crSvc.addCourseReservation(crVO.getTeacherId(), crVO.getMemId(), crVO.getInscId(), crVO.getTeamId(),
+						crVO.getCrvStatus(), crVO.getClassStatus(), crVO.getTranStatus(), crVO.getCrvMFD(),
+						crVO.getCrvEXP(), crVO.getCrvLoc(), crVO.getCrvTotalTime(), crVO.getCrvTotalPrice(),
+						crVO.getCrvRate());
+
+				// 刪除已無的時間
+				insctSvc.deleteInsCourseTime(crVO.getInscTimeId());
+				// 新增成功
+				// Android處理
+				if ("make_new_reservation".equals(action)) {
+					String success = "";
+					out.print(success);
+
+					// Web導向
+				} else {
 					String url = "/coursereservation/courseOrder.jsp";
-					req.setAttribute("courseReservationVO", courseReservationVO); 
-					RequestDispatcher successView = req.getRequestDispatcher(url); // 
-					successView.forward(req, res);				
-					
-					/***************************其他可能的錯誤處理**********************************/
-				} 
-				catch (Exception e) {
-					errorMsgs.add(e.getMessage());
-					RequestDispatcher failureView = req
-							.getRequestDispatcher("/insCourseServlet/insCourseDetails.jsp");
-					failureView.forward(req, res);
+					req.setAttribute("courseReservationVO", crVO);
+					RequestDispatcher successView = req.getRequestDispatcher(url);
+					successView.forward(req, res);
 				}
+				// 已經被搶走
+			} else {
+				// Android處理
+				if ("make_new_reservation".equals(action)) {
+					String error = "error";
+					out.print(error);
+					// Web處理
+				} else {
+					req.setAttribute("courseReservationVO", crVO); // 含有輸入格式錯誤的empVO物件,也存入req
+					RequestDispatcher failureView = req.getRequestDispatcher("/coursereservation/insCourseDetails.jsp");
+					failureView.forward(req, res);
+					return;
+				}
+
+			}
+
+			}
 			
-		  }
-				
-		
-		
+			
+			}	
+
 		
 
-	
-	
-	
-				
-	}	
+		// 取消
+		if ("cancel_my_reservation".equals(action)) {
+		}
+
+	}
+
 }
